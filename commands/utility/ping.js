296da8af0a55b470155bc67f4c27e69ba29a6a96
k2angel <90847045+k2angel@users.noreply.github.com>
2026-01-26 20:15:40 +0900

    style: remove unnecessary commas

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -4,5 +4,5 @@ module.exports = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
-	},
+	}
 };
